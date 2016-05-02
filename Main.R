e857406d105f0e21f48a7582498c7b8344967202
mackg867 <mackenzie.ginithan@gmail.com>
2016-05-01 18:52:58 -0700

    Update Main.R

--- Main.R
+++ Main.R
@@ -2,15 +2,24 @@ setwd("C:/Users/Computer/Desktop/Kaggle/Animal Shelter Competition")
 source("Models.R")
 load("test_clean.RData")
 
+######
 #Build Random Forest Model
+######
 forest = build_RF() #Accuracy: .6307
 
+######
 #Build Naive Bayes
+######
 bayes = build_Bayes() #Accuracy: 0.6034
 
+######
 #Build Neural Net
+######
 net = build_Net() #Accuracy: 0.6325
-#Build GBM 
+
+######
+#Build GBM Model and Predict the Test Data
+######
 g_boost = build_GBM() #Accuracy: 0.635
 preds = predict(g_boost,newdata=test,type="response")
 submission = as.data.frame(preds[,,1])
