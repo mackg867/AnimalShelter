25e7694fb168be7484e742d8e3bc4eace95374b5
mackg867 <mackenzie.ginithan@gmail.com>
2016-05-03 14:19:00 -0700

    Added files via upload

--- Main.R
+++ Main.R
@@ -29,6 +29,6 @@ source("Models.R")
 ######
 #Build Logistic Model and Predict the Test Data
 ######
-  logistic = build_logistic() #Accuracy:6154
+  logistic = build_logistic() #Accuracy: 0.6154
 
 
