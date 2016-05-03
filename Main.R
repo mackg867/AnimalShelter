4cda1af35b4562cc9bc24f19cec2f8680613c3ce
mackg867 <mackenzie.ginithan@gmail.com>
2016-05-03 09:01:54 -0700

    Update Main.R

--- Main.R
+++ Main.R
@@ -5,27 +5,27 @@ load("test_clean.RData")
 ######
 #Build Random Forest Model
 ######
-forest = build_RF() #Accuracy: .6307
+  forest = build_RF() #Accuracy: .6307
 
 ######
 #Build Naive Bayes
 ######
-bayes = build_Bayes() #Accuracy: 0.6034
+  bayes = build_Bayes() #Accuracy: 0.6034
 
 ######
 #Build Neural Net
 ######
-net = build_Net() #Accuracy: 0.6325
+  net = build_Net() #Accuracy: 0.6325
 
 ######
 #Build GBM Model and Predict the Test Data
 ######
-g_boost = build_GBM() #Accuracy: 0.635
-preds = predict(g_boost,newdata=test,type="response")
-submission = as.data.frame(preds[,,1])
-submission = cbind(test$AnimalID,submission)
-names(submission)[1] = "ID"
-write.csv(submission,file="gbm_submission.csv",sep=",",row.names=FALSE)
+  g_boost = build_GBM() #Accuracy: 0.635
+  preds = predict(g_boost,newdata=test,type="response")
+  submission = as.data.frame(preds[,,1])
+  submission = cbind(test$AnimalID,submission)
+  names(submission)[1] = "ID"
+  write.csv(submission,file="gbm_submission.csv",sep=",",row.names=FALSE)
 
 
 
